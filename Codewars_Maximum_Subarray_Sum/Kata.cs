86475c52eb59d0329bd9f83293219bdecd3fd6dc
Maralov Kemal <kemo181098@gmail.com>
2020-11-30 23:54:59 +0600

    Add project files.

---
+++ Codewars_Maximum_Subarray_Sum/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Codewars_Maximum_Subarray_Sum
+{
+    public static class Kata
+    {
+        /// <summary>
+        /// This method finds the maximum sum of a contiguous subsequence in an array or list of integers
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns>it return the largest possible sum</returns>
+        public static int MaxSequence(int[] arr)
+        {
+            if(arr.Length.Equals(0))
+            {
+                return 0;
+            }
+
+            int bestSum = 0;
+            int currentSum = 0;
+
+            foreach(var x in arr)
+            {
+                currentSum = Math.Max(0, currentSum + x);
+                bestSum = Math.Max(bestSum, currentSum);
+            }
+
+            return bestSum;
+        }
+    }
+}
---
+++ Codewars_Maximum_Subarray_Sum_Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Codewars_Maximum_Subarray_Sum;
+using NUnit.Framework;
+
+namespace Codewars_Maximum_Subarray_Sum_Test
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test0()
+        {
+            Assert.AreEqual(0, Kata.MaxSequence(new int[0]));
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(6, Kata.MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+    }
+}
